3323a710647a192e440334ece5497381b0bc030e
Keval Patel <200383118@student.georgianc.on.ca>
2019-06-16 11:50:58 -0400

    views added

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,21 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Index' });
 });
-
-router.get('/',function(req,res,next){
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'Index' });
+});
+router.get('/about',function(req,res,next){
+	res.render('about',{title:'About Me'});
+});
+router.get('/projects',function(req,res,next){
+	res.render('projects',{title:'Projects'});
+});
+router.get('/services',function(req,res,next){
+	res.render('services',{title:'Services'});
+});
+router.get('/contact',function(req,res,next){
 	res.render('contact',{title:'Contact Me'});
 });
+
 
 module.exports = router;
